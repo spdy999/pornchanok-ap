be2bbae3ea3573f8fdf53724dc29a86d34080c7f
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-07-26 05:52:19 +0700

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  useMediaQuery,
+  createMuiTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@material-ui/core';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
