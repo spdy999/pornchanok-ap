ad0e0dc68509b409f61ffd6cf4b57a4b74ed4f7b
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-07-26 08:33:39 +0700

    add tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import {
-  useMediaQuery,
-  createMuiTheme,
-  CssBaseline,
-  ThemeProvider,
-} from '@material-ui/core';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import { createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core'
+import React from 'react'
+import './App.css'
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
 
   const theme = React.useMemo(
     () =>
       createMuiTheme({
         palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-        },
+          type: prefersDarkMode ? 'dark' : 'light'
+        }
       }),
-    [prefersDarkMode],
-  );
+    [prefersDarkMode]
+  )
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
