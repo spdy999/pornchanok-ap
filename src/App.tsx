1f0702af3ee880dde1137f9f2a47081641a93ba0
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-07-26 08:45:26 +0700

    Squashed commit of the following:
    commit 5657339715719582a00154168d74f56094dd5d43
    Author: spdy999 <1a1a1a.262626.0010@gmail.com>
    Date:   Sun Jul 26 08:35:58 2020 +0700
    
        remove tslint-etc
    
    commit ad0e0dc68509b409f61ffd6cf4b57a4b74ed4f7b
    Author: spdy999 <1a1a1a.262626.0010@gmail.com>
    Date:   Sun Jul 26 08:33:39 2020 +0700
    
        add tslint
    
    commit be2bbae3ea3573f8fdf53724dc29a86d34080c7f
    Author: spdy999 <1a1a1a.262626.0010@gmail.com>
    Date:   Sun Jul 26 05:52:19 2020 +0700
    
        add dark mode
    
    commit 47dcfc9aa98b75667f32dbae094cc3ddfa09e787
    Author: spdy999 <1a1a1a.262626.0010@gmail.com>
    Date:   Sun Jul 26 05:44:49 2020 +0700
    
        add responsive meta
    
    commit bb3f1ea3fbb729113819fd5cc21bb277411fc11c
    Author: spdy999 <1a1a1a.262626.0010@gmail.com>
    Date:   Sun Jul 26 04:09:49 2020 +0700
    
        finish add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core'
+import React from 'react'
+import './App.css'
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light'
+        }
+      }),
+    [prefersDarkMode]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
