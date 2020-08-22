f112e1cacec35a86d2126798077bb8907af80ac5
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-22 11:40:48 +0700

    remove dirty lines

--- src/components/FullPages.tsx
+++ src/components/FullPages.tsx
@@ -1,36 +1,16 @@
 import ReactFullpage from '@fullpage/react-fullpage'
-import { makeStyles } from '@material-ui/core'
 import React from 'react'
-
-const useStyles = makeStyles(theme => ({
-  chair: {
-    height: '60%',
-    position: 'absolute',
-    top: '70%',
-    left: '50%',
-    transform: 'translate(-50%, -70%)',
-  },
-}))
 
 interface FullPagesProps {}
 
 export const FullPages: React.FC<FullPagesProps> = props => {
-  const classes = useStyles()
   interface IfullpageApi {
     moveTo: (a: number, b: number) => void
   }
-  //   const onLeave = (origin, destination, direction) => {
-  //     console.log('Leaving section ' + origin.index)
-  //   }
-  //   const afterLoad = (origin, destination, direction) => {
-  //     console.log('After load: ' + destination.index)
-  //   }
   return (
     <ReactFullpage
       scrollOverflow={true}
       sectionsColor={['orange', 'purple', 'green']}
-      //   onLeave={this.onLeave.bind(this)}
-      //   afterLoad={this.afterLoad.bind(this)}
       render={props => {
         return (
           <div id="fullpage-wrapper">
