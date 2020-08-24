b3b61b007e4ed5e3d5fbd06f7eb107e4ce73270d
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-24 07:17:53 +0700

    add StyledTextLink

--- src/components/Sections/ContactSection.tsx
+++ src/components/Sections/ContactSection.tsx
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core'
 import React from 'react'
 import StyledTextH2 from '../common/StyledTextH2'
 import StyledTextH3 from '../common/StyledTextH3'
+import StyledTextLink from '../common/StyledTextLink'
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -18,6 +19,7 @@ export const ContactSection: React.FC<ContactSectionProps> = props => {
       <div className={classes.root}>
         <StyledTextH2>CONTACT</StyledTextH2>
         <StyledTextH3>Feel free to contact me.</StyledTextH3>
+        <StyledTextLink>Pornchanok.A@alumni.chula.ac.th</StyledTextLink>
       </div>
     </div>
   )
