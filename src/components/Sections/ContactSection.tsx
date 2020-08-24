3285e18f2c8bee87822b4a6d9dcd3721c37a68ac
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-24 08:47:58 +0700

    icon spacing

--- src/components/Sections/ContactSection.tsx
+++ src/components/Sections/ContactSection.tsx
@@ -1,16 +1,23 @@
-import { ListItemIcon, makeStyles } from '@material-ui/core'
+import { Grid, GridSpacing, makeStyles } from '@material-ui/core'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import React from 'react'
+import React, { useState } from 'react'
 import StyledTextH2 from '../common/StyledTextH2'
 import StyledTextH3 from '../common/StyledTextH3'
 import StyledTextLink from '../common/StyledTextLink'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {
     color: '#0f0',
     textAlign: 'center',
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(9),
   },
   icon: {
     minWidth: '30px',
@@ -20,6 +27,8 @@ const useStyles = makeStyles(() => ({
 interface ContactSectionProps {}
 export const ContactSection: React.FC<ContactSectionProps> = props => {
   const classes = useStyles()
+  const [spacing] = useState<GridSpacing>(2)
+
   return (
     <div className="section">
       <div className={classes.root}>
@@ -27,11 +36,19 @@ export const ContactSection: React.FC<ContactSectionProps> = props => {
         <StyledTextH3>Feel free to contact me.</StyledTextH3>
         <StyledTextLink>Pornchanok.A@alumni.chula.ac.th</StyledTextLink>
         <StyledTextH3>Tel: +66917689666</StyledTextH3>
-        <ListItemIcon className={classes.icon}>
-          <GitHubIcon />
-          <LinkedInIcon />
-          <FacebookIcon />
-        </ListItemIcon>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={spacing}>
+            <Grid item>
+              <GitHubIcon />
+            </Grid>
+            <Grid item>
+              <LinkedInIcon />
+            </Grid>
+            <Grid item>
+              <FacebookIcon />
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     </div>
   )
