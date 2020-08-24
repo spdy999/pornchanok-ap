d04b4456692503d34f6e2a616c5a910603340c54
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-24 08:26:17 +0700

    add icons

--- src/components/Sections/ContactSection.tsx
+++ src/components/Sections/ContactSection.tsx
@@ -1,4 +1,7 @@
-import { makeStyles } from '@material-ui/core'
+import { ListItemIcon, makeStyles } from '@material-ui/core'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import React from 'react'
 import StyledTextH2 from '../common/StyledTextH2'
 import StyledTextH3 from '../common/StyledTextH3'
@@ -8,6 +11,9 @@ const useStyles = makeStyles(() => ({
   root: {
     color: '#0f0',
     textAlign: 'center',
+  },
+  icon: {
+    minWidth: '30px',
   },
 }))
 
@@ -20,6 +26,12 @@ export const ContactSection: React.FC<ContactSectionProps> = props => {
         <StyledTextH2>CONTACT</StyledTextH2>
         <StyledTextH3>Feel free to contact me.</StyledTextH3>
         <StyledTextLink>Pornchanok.A@alumni.chula.ac.th</StyledTextLink>
+        <StyledTextH3>Tel: +66917689666</StyledTextH3>
+        <ListItemIcon className={classes.icon}>
+          <GitHubIcon />
+          <LinkedInIcon />
+          <FacebookIcon />
+        </ListItemIcon>
       </div>
     </div>
   )
