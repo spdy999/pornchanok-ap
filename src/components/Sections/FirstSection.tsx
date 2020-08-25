bd034f780fd23f4eb62c08d0e4279d90bc339be9
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-26 04:03:43 +0700

    add my name to section

--- src/components/Sections/FirstSection.tsx
+++ src/components/Sections/FirstSection.tsx
@@ -1,12 +1,26 @@
+import { Container, makeStyles } from '@material-ui/core'
 import React from 'react'
+import StyledTextH1 from '../common/StyledTextH1'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingLeft: '30%',
+  },
+}))
 
 interface FirstSectionProps {}
 export const FirstSection: React.FC<FirstSectionProps> = props => {
+  const classes = useStyles()
+
   return (
     <div className="section section1">
-      <h3>I'M</h3>
-      <h3>PORNCHANOK</h3>
-      <h3>AP.</h3>
+      <Container className={classes.container}>
+        <StyledTextH1>I'M</StyledTextH1>
+        <StyledTextH1>PORNCHANOK</StyledTextH1>
+        <StyledTextH1>AP.</StyledTextH1>
+      </Container>
     </div>
   )
 }
