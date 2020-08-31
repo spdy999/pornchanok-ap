4ec32f604255aeb027b4275ccfc90628558a5177
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-08-31 17:25:50 +0700

    style special text and spacing

--- src/components/Sections/FirstSection.tsx
+++ src/components/Sections/FirstSection.tsx
@@ -1,14 +1,28 @@
-import { Container, makeStyles } from '@material-ui/core'
+import { Box, Container, makeStyles } from '@material-ui/core'
+import { withStyles } from '@material-ui/styles'
 import React from 'react'
-import StyledTextH1 from '../common/StyledTextH1'
+import StyledTextH2 from '../common/StyledTextH2'
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    paddingLeft: '30%',
+    paddingLeft: '10%',
+    fontWeight: 500,
+    alignContent: 'space-between',
   },
 }))
+
+const StyledNameText = withStyles({
+  root: {
+    fontWeight: 'bold',
+    fontSize: '3em',
+    // fontSize: 70,
+    m: 1,
+    textOverflow: 'ellipsis',
+    component: 'div',
+  },
+})(Box)
 
 interface FirstSectionProps {}
 export const FirstSection: React.FC<FirstSectionProps> = props => {
@@ -17,9 +31,16 @@ export const FirstSection: React.FC<FirstSectionProps> = props => {
   return (
     <div className="section section1">
       <Container className={classes.container}>
-        <StyledTextH1>I'M</StyledTextH1>
-        <StyledTextH1>PORNCHANOK</StyledTextH1>
-        <StyledTextH1>AP.</StyledTextH1>
+        <Box>
+          <StyledNameText>I'M</StyledNameText>
+          <StyledNameText>PORNCHANOK</StyledNameText>
+          <StyledNameText>AP.</StyledNameText>
+        </Box>
+        <Box mb={30}></Box>
+        <Box>
+          <StyledTextH2>WEB & MOBILE</StyledTextH2>
+          <StyledTextH2>DEVELOPER</StyledTextH2>
+        </Box>
       </Container>
     </div>
   )
