f1ca81c175f2e6ce07a31e0d13a664d01e9b9a06
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-09-01 15:06:49 +0700

    Updates

---
+++ precache-manifest.cd4e9dfc8b833afc18075a6fbbe4d871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491ab4935daf0712d1d723e1100a5253",
+    "url": "/pornchanok-ap/index.html"
+  },
+  {
+    "revision": "4ba66c2e6676916fd5d8",
+    "url": "/pornchanok-ap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "06a6bd5a29fd12c3f474",
+    "url": "/pornchanok-ap/static/js/2.45a112bb.chunk.js"
+  },
+  {
+    "revision": "7a027744581ad182eb9c51f4f432b94a",
+    "url": "/pornchanok-ap/static/js/2.45a112bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba66c2e6676916fd5d8",
+    "url": "/pornchanok-ap/static/js/main.d066e221.chunk.js"
+  },
+  {
+    "revision": "0dba912c7be61e7509f7",
+    "url": "/pornchanok-ap/static/js/runtime-main.71de96a6.js"
+  }
+]);
